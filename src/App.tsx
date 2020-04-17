dd29c98f6488a5082c4a6fb6f75bd7306f6228da
ColTo <ctopper@uw.edu>
2020-04-16 17:25:59 -0700

    Initial commit with bootstrap code from create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">BootStrap Placeholder</header>
+        </div>
+    );
+}
+
+export default App;
