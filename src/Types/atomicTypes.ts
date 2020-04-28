7b2298f82a08552c433e0a271b97eb6d3b57f255
Colin Topper <coltopper@hotmail.com>
2020-04-28 14:08:39 -0700

    Api contracts (#17)
    * Added dependency to rascal for queue integration
    
    * Initial model for API requests and responses from frontend
    
    * disambiguated projection to use for cordinates, updated contracts to match Google Maps API

---
+++ src/Types/atomicTypes.ts
@@ -0,0 +1,73 @@
+export enum State {
+    Alaska = "AK",
+    Alabama = "AL",
+    Arkansas = "AR",
+    Arizona = "AZ",
+    California = "CA",
+    Colorado = "CO",
+    Connecticut = "CT",
+    Delaware = "DE",
+    Florida = "FL",
+    Georgia = "GA",
+    Hawaii = "HI",
+    Iowa = "IA",
+    Idaho = "ID",
+    Illinois = "IL",
+    Indiana = "IN",
+    Kansas = "KS",
+    Kentucky = "KY",
+    Louisiana = "LA",
+    Massachusetts = "MA",
+    Maryland = "MD",
+    Maine = "ME",
+    Michigan = "MI",
+    Minnesota = "MN",
+    Missouri = "MO",
+    Mississippi = "MS",
+    Montana = "MT",
+    NorthCarolina = "NC",
+    NorthDakota = "ND",
+    Nebraska = "NE",
+    NewHampshire = "NH",
+    NewJersey = "NJ",
+    NewMexico = "NM",
+    Nevada = "NV",
+    NewYork = "NY",
+    Ohio = "OH",
+    Oklahoma = "OK",
+    Oregon = "OR",
+    Pennsylvania = "PA",
+    RhodeIsland = "RI",
+    SouthCarolina = "SC",
+    SouthDakota = "SD",
+    Tennessee = "TN",
+    Texas = "TX",
+    Utah = "UT",
+    Virginia = "VA",
+    Vermont = "VT",
+    Washington = "WA",
+    Wisconsin = "WI",
+    WestVirginia = "WV",
+    Wyoming = "WY",
+}
+
+export interface Precinct {
+    name: string;
+    id: PrecinctID;
+    vertices: Cordinate[];
+    district: DistrictID;
+}
+
+export interface Cordinate {
+    lat: number;
+    lng: number;
+}
+
+export enum JobType {
+    StartMap = "StartMap",
+    ScoreMap = "ScoreMap",
+}
+
+export type PrecinctID = number;
+export type DistrictID = number;
+export type MapID = number;
