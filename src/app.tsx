b67ca33e68a3689b16aa0da8d68ec829f309284a
ColTo <ctopper@uw.edu>
2020-05-04 10:42:20 -0700

    force camelCase

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+export class App extends React.Component {
+    render(): JSX.Element {
+        return (
+            <div className="App">
+                <header className="App-header">BootStrap Placeholder</header>
+            </div>
+        );
+    }
+}
