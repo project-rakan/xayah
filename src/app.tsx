7e0e2a6345ade6e299f47df0356921ee3f07a0b4
Colin Topper <coltopper@hotmail.com>
2020-05-11 21:08:09 -0700

    rakan provider imp, needs config and testing

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./app.css";
+import { rascalRakanProvider } from "./providers/rakanProvider/rascalRakanProvider";
+import { State } from "./types";
 
 export class App extends React.Component {
     render(): JSX.Element {
+        rascalRakanProvider.startMapJob({
+            id: "PLACEHOLDER",
+            state: State.Iowa,
+            alpha: 1,
+            beta: 1,
+            gamma: 1,
+            eta: 1,
+        });
         return (
             <div className="App">
                 <header className="App-header">BootStrap Placeholder</header>
--- src/providers/rakanProvider/rascalRakanProvider.ts
+++ src/providers/rakanProvider/rascalRakanProvider.ts
@@ -1,14 +1,68 @@
-import { RakanProvider, StartMapJobRequest, ScoreMapRequest } from "./types";
+import {
+    RakanProvider,
+    StartMapJobRequest,
+    ScoreMapRequest,
+    MapJobUpdate,
+} from "./types";
 import { addMapJob } from "../../redux/mapJobs/actionCreators";
+import { AckOrNackFn, BrokerAsPromised, withDefaultConfig } from "rascal";
 
 class RascalRakanProvider implements RakanProvider {
     // TODO This class ought to have a static singleton broker - see the rascal Callbacks example
     // TODO The broker needs to be subsribed to the queue and provided with a callback using MapJobUpdate()
     // See mockRakan Provider for redux actioncreator usage.
 
-    startMapJob(request: StartMapJobRequest): void {
-        // TODO send the provided request to Rakan over the queue using rascal.
+    static config = require("./config");
 
+    private requests: (StartMapJobRequest | ScoreMapRequest)[] = [];
+
+    setup = async (): Promise<void> => {
+        try {
+            const broker = await BrokerAsPromised.create(
+                withDefaultConfig(RascalRakanProvider.config)
+            );
+            broker.on("error", console.error);
+
+            try {
+                const subscription = await broker.subscribe("xayah");
+                subscription
+                    .on("message", function (
+                        message,
+                        content: MapJobUpdate,
+                        ackOrNack: AckOrNackFn
+                    ) {
+                        console.log(content);
+                        ackOrNack();
+                    })
+                    .on("error", console.error);
+            } catch (err) {
+                console.error(err);
+            }
+
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            setInterval(async (): Promise<void> => {
+                try {
+                    if (this.requests) {
+                        const publication = await broker.publish(
+                            "rakan",
+                            this.requests.pop()
+                        );
+                        publication.on("error", console.error);
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+            }, 1000);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    constructor() {
+        this.setup();
+    }
+
+    startMapJob(request: StartMapJobRequest): void {
         // This adds the map job to our local mapjob redux store with an empty district map.
         // The Map will be updated via callbacks
         addMapJob({
@@ -20,6 +74,9 @@ class RascalRakanProvider implements RakanProvider {
             eta: request.eta,
             map: new Map(),
         });
+
+        // This adds the request to the provider and marks it as being ready to be broadcast over the queue
+        this.requests.push(request);
     }
 
     // Out of scope for beta release
