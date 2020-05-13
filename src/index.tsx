3bf5866311ac80dff98bdd314d1ea8606cf93c05
Colin Topper <coltopper@hotmail.com>
2020-05-12 18:17:21 -0700

    implemented axiosRakanProvider, just needs urls

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { connectedRouter as Router } from "./components/router";
-import { rheaRakanProvider } from "./providers/rakanProvider/rheaRakanProvider";
-import { State } from "./types";
 
 ReactDOM.render(
     <Provider store={store}>
@@ -14,15 +12,6 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root")
 );
-
-rheaRakanProvider.startMapJob({
-    alpha: 0,
-    beta: 0,
-    gamma: 0,
-    eta: 0,
-    id: "1",
-    state: State.Iowa,
-});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
